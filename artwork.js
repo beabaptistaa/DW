f917630c78d2511c19ce6796e0976b183da94c55
beabaptistaa <68157224+beabaptistaa@users.noreply.github.com>
2022-11-07 09:53:33 +0000

    Create artwork.js

---
+++ artwork.js
@@ -0,0 +1,9 @@
+{
+    "artworks": [
+        {
+            "id": 27992,
+            "title": "A Sunday on La Grande Jatte — 1884",
+            "image_id": "1adf2696-8489-499b-cad2-821d7fde4b33"
+        }
+    ]
+}
