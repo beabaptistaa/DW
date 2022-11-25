2c8fad7072668e2a6520b554a0be037b677a9fb9
beabaptistaa <68157224+beabaptistaa@users.noreply.github.com>
2022-11-25 21:25:21 +0000

    Delete artwork.js

--- artwork.js
+++
@@ -1,8 +0,0 @@
-class Artwork {
-    constructor(artworkContainer, imageUrl) {
-      // Same as document.createElement('img');
-      const image = new Image();
-      image.src = imageUrl;
-      artworkContainer.append(image);
-    }
-  }
